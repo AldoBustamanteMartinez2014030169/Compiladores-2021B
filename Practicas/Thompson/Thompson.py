0c6965232437893b666a1cbccb0b10b611b0b322
AldoBustamanteMartinez2014030169 <85587168+AldoBustamanteMartinez2014030169@users.noreply.github.com>
2021-06-20 13:14:59 -0500

    Add files via upload

--- Practicas/Thompson/Thompson.py
+++ Practicas/Thompson/Thompson.py
@@ -1,10 +1,12 @@
 
 # Thompson
 
-# a, b, |, *
-# a, a*, ab, abab
+# a, b, ab, ba, aba, bab, a|b, b|a, ab|a, b|ba, aaa|bbb
 
 def grafo(cadena, lista, inicial, final):
+
+	bandera=0
+	bandera2=0
 
 	for x in range(len(cadena)):
 
@@ -14,14 +16,23 @@ def grafo(cadena, lista, inicial, final):
 				final=2
 			else:
 				anterior=lista[-1]
-				lista.append([anterior[1], anterior[1]+1, cadena[x]])
-				final=anterior[1]+1
+				if bandera and bandera2==0:
+					lista.append([inicial, anterior[1]+1, 'E'])
+					lista.append([anterior[1]+1, anterior[1]+2, cadena[x]])
+					bandera2=1
+				elif bandera and bandera2==1:
+					lista.append([anterior[1], anterior[1]+1, cadena[x]])
+					lista.append([anterior[1]+1, final, 'E'])
+				elif bandera==0 and bandera2==0:
+					lista.append([anterior[1], anterior[1]+1, cadena[x]])
+					final=anterior[1]+1
 		else:
 			anterior=lista[-1]
 			lista.append([anterior[1]+1, inicial, 'E'])
 			lista.append([anterior[1], anterior[1]+2, 'E'])
 			inicial=anterior[1]+1
 			final=anterior[1]+2
+			bandera=1
 			# subcadena=cadena[x:-1]
 			# grafo(subcadena, lista, inicial, final)
 
