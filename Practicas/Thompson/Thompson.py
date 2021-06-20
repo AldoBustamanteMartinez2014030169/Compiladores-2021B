396c70a956a53084366fc03a4ef6e032efcaa4c5
AldoBustamanteMartinez2014030169 <85587168+AldoBustamanteMartinez2014030169@users.noreply.github.com>
2021-06-20 12:52:42 -0500

    Add files via upload

--- Practicas/Thompson/Thompson.py
+++ Practicas/Thompson/Thompson.py
@@ -4,28 +4,39 @@
 # a, b, |, *
 # a, a*, ab, abab
 
+def grafo(cadena, lista, inicial, final):
+
+	for x in range(len(cadena)):
+
+		if cadena[x]!='|':
+			if x==0:
+				lista.append([1, 2, cadena[x]])
+				final=2
+			else:
+				anterior=lista[-1]
+				lista.append([anterior[1], anterior[1]+1, cadena[x]])
+				final=anterior[1]+1
+		else:
+			anterior=lista[-1]
+			lista.append([anterior[1]+1, inicial, 'E'])
+			lista.append([anterior[1], anterior[1]+2, 'E'])
+			inicial=anterior[1]+1
+			final=anterior[1]+2
+			# subcadena=cadena[x:-1]
+			# grafo(subcadena, lista, inicial, final)
+
+
 lista=[]
 inicial=1
 final=1
 
-cadena="ab"
+cadena="ab|ba"
 
-for x in range(len(cadena)):
-
-	if cadena[x]!='|':
-		if x==0:
-			lista.append([1, 2, cadena[x]])
-			final=2
-		else:
-			anterior=lista[-1]
-			lista.append([anterior[1], anterior[1]+1, cadena[x]])
-			final=anterior[1]+1
-	
+grafo(cadena, lista, inicial, final)
 
 for estados in lista:
 	print(estados)
 
 print("Estado inicial: ", inicial)
 print("Estado final: ", final)
-	# if cadena[x]=='|'
 
